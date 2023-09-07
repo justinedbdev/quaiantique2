e2c21a0e024ede9b1afa44adff39c6cf04222edd
Justine Debard <justine_debard@yahoo.com>
2023-09-07 17:56:30 +0200

    Edit sql file

--- SQLFilesBDD/AvisCreationBDD.sql
+++ SQLFilesBDD/AvisCreationBDD.sql
@@ -5,11 +5,18 @@ CREATE TABLE
         contenu varchar(255) NOT NULL,
         client_id int NOT NULL,
         foreign key (client_id) REFERENCES client(client_id)
-    );
+
+CONSTRAINT fk_client_id FOREIGN KEY (client_id) REFERENCES client (client_id),
+);
 
 INSERT INTO
     Avis (note, contenu, client_id)
-VALUES 
-    (5, 'Le service est rapide et génial, nous avons toujours été bien accueillis!', 52),
-    (4, 'Lieu très chaleureux. Convient à des groupes d\'amis. Les repas sont très bon et les formules sont abordables et permet de goûter a plusieurs spécialités savoyardes.', 123),
-    (5, 'Tout était parfait!', 189),
+VALUES (
+        5,
+        'Le service est rapide et génial, nous avons toujours été bien accueillis!',
+        52
+    ), (
+        4,
+        'Lieu très chaleureux. Convient à des groupes d\'amis. Les repas sont très bon et les formules sont abordables et permet de goûter a plusieurs spécialités savoyardes.',
+        123
+    ), (5, 'Tout était parfait!', 189),
